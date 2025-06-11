e3db2e8b4618d6dd8301e989a6edca9f784962d3
fuzfuz <149338034+beanduan22@users.noreply.github.com>
2025-06-12 07:59:29 +1000

    Update utils.py

--- bayeswarp/lenet4/utils.py
+++ bayeswarp/lenet4/utils.py
@@ -1,20 +1,19 @@
-# bayeswarp/utils.py
 
 import torch
 
 def to_device(tensor, device):
     return tensor.to(device)
 
-def set_seed(seed=42):
+def set_seed(seed=seed):
     import random, numpy as np
     torch.manual_seed(seed)
     random.seed(seed)
     np.random.seed(seed)
 
 def load_model(path, device="cuda"):
-    # 加载模型权重
+
     state = torch.load(path, map_location=device)
 
-    model = ...  # 实例化你的模型
+    model = ...  # Instantiate your model
     model.load_state_dict(state)
     return model.to(device)
